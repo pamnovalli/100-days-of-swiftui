a2186e94a3dcd9cfd48399fdf00ea516afb2d5e0
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-09-15 00:11:22 -0300

    Added project-three

---
+++ Projects/project-three/project-three/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-three
+//
+//  Created by Pamela Ianovalli on 14/09/20.
+//  Copyright © 2020 Pamela Ianovalli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
