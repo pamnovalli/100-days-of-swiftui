b21e76689f4c2d4bc8081fe587baec49df19cdf7
pamnovalli <pamela.novalli@gmail.com>
2020-11-16 21:56:36 -0300

    added project nine

---
+++ Projects/project-nine/project-nine/project_nineApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_nineApp.swift
+//  project-nine
+//
+//  Created by Pamela Ianovalli on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct project_nineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
